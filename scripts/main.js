c8c4d6195ece3a156eb6b5c2e1ebdcff89b11a12
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-08-12 23:02:20 +0200

    created loader in html animated with css and load by js + start of construction of a mobile menu, navbar responsive  800px above

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
+// LOADER
 
+window.addEventListener("load", () => {
+  document.querySelector(".loader-container").style.display = "none";
+  document.body.style.overflow = "auto";
+  document.body.style.background = "rgb(25,1,31)";
+});
