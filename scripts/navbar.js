7f1f02ca31638b61da4a38f5c3afa1c13a28b68b
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-08-26 19:24:55 +0200

    trying fixing navbar, bad result, save for a soft reset

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,12 +1,19 @@
+// hide navbar when scroll down and show when scroll up
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-  let navbar = document.getElementById("navbar");
+  let navbar = document.querySelector("header");
+
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     navbar.style.top = "0";
-    navbar.style.zIndex = "1";
+    navbar.style.position = "fixed";
   } else {
     navbar.style.top = "-100px";
   }
   prevScrollpos = currentScrollPos;
+  if (currentScrollPos == "0") {
+    navbar.style.boxShadow = "none";
+    navbar.style.transition = "0.7s";
+  }
 };
+console.log(window.pageXOffset);
