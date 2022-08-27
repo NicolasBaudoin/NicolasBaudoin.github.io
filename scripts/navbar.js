df2c36c34c19ad945c54c046e27943e5c9227f6f
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-08-27 13:20:26 +0200

    Fixed navbar overlap, add boxshadow style on navbar when scroll down and remove same when user is at top of the page center main

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,22 +1,20 @@
-// // hide navbar when scroll down and show when scroll up
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function () {
-//   let navbar = document.querySelector("header");
+var prevScrollpos = window.pageYOffset;
+var navbar = document.getElementById("navbar");
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = "0";
+    navbar.classList.add("shadow");
+  } else {
+    navbar.style.top = "-100px";
+  }
+  if (currentScrollPos == "0") {
+    navbar.classList.remove("shadow");
+  }
+  prevScrollpos = currentScrollPos;
+};
 
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     navbar.style.top = "0";
-//     navbar.style.position = "fixed";
-//   } else {
-//     navbar.style.top = "-100px";
-//   }
-//   prevScrollpos = currentScrollPos;
-//   if (currentScrollPos == "0") {
-//     navbar.style.boxShadow = "none";
-//     navbar.style.transition = "0.7s";
-//   }
-// };
-// console.log(window.pageXOffset);
+// ===== MOBILE NAVBAR =====
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
 
