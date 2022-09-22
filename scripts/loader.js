e4012fd70c1f7158abe185fdec8d0f790ff7ef32
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-09-22 17:32:32 +0200

    🚚 Remove loader code
    remove loader html, css and js

--- scripts/loader.js
+++
@@ -1,6 +0,0 @@
-// LOADER
-
-// window.addEventListener("load", () => {
-//   document.querySelector(".loader-container").style.display = "none";
-//   document.body.style.overflow = "auto";
-// });
