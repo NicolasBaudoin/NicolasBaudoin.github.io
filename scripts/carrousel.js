107453bd5c23a4c8bc586606c1a9b371f93725a5
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-10-02 19:44:36 +0200

    🔧 Carousel JS
     cancel translation jquery to vanilla js
     setup btn variables
     create function log to test without writing "console.log()" but "log()"
    
    Explain :
    Tried to translate jquery code to vanilla js but i have not enough experiences to do it correctly
    so i will code a carousel vanilla by myself (with internet's ressources)

--- scripts/carrousel.js
+++ scripts/carrousel.js
@@ -1,44 +1,7 @@
-var carrousel = document.querySelector(".carousel");
-var seat = document.querySelector(".carousel-seat");
+const PREVBTN = document.getElementById("prev");
+const NEXTBTN = document.getElementById("next");
 
-$(".toggle").on("click", function (e) {
-  var newSeat;
-  var elements = document.querySelector(".is-ref");
-  var currSlideControl = $(e.currentTarget);
-  // e.target is what triggers the event dispatcher to trigger and e.currentTarget is what you assigned your listener to.
-
-  elements.removeClass("is-ref");
-  if (currSlideControl.data("toggle") === "next") {
-    newSeat = next(elements);
-    carrousel.removeClass("is-reversing");
-  } else {
-    newSeat = prev(elements);
-    carrousel.addClass("is-reversing");
-  }
-
-  newSeat.addClass("is-ref").css("order", 1);
-  for (var i = 2; i <= seat.length; i++) {
-    newSeat = next(newSeat).css("order", i);
-  }
-
-  carrousel.removeClass("is-set");
-  return setTimeout(function () {
-    return carrousel.addClass("is-set");
-  }, 50);
-
-  function next(elements) {
-    if (elements.next().length) {
-      return elements.next();
-    } else {
-      return seat.first();
-    }
-  }
-
-  function prev(elements) {
-    if (elements.prev().length) {
-      return elements.prev();
-    } else {
-      return seat.last();
-    }
-  }
-});
+// DEBUG
+function log(e) {
+  console.log(e);
+}
